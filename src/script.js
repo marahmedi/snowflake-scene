a914ca92c8a285ced7bbef88e52b0cf844b101fa
Marah Abughazza <marah05@live.com>
2023-07-27 14:48:00 +0100

    added snowflake animation

--- src/script.js
+++ src/script.js
@@ -1,14 +1,22 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'lil-gui'
+
 
 THREE.ColorManagement.enabled = false
+
+// constants
+
+let particles;
+let positions = [], velocities = []
+const count = 30000
+const maxRange = 1000, minRange = maxRange/2;
+const minHeight = 150;
+
 
 /**
  * Base
  */
-// Debug
-const gui = new dat.GUI()
+
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -35,14 +43,14 @@ const snowflake = textureLoader.load('../textures/particles/snowflake.png')
  * Sphere
  */
 
-const geometry = new THREE.SphereGeometry(30, 64, 64)
+const geometry = new THREE.SphereGeometry(40, 20, 50)
 const material = new THREE.MeshBasicMaterial({
     map: background
 })
 
-const mesh = new THREE.Mesh(geometry, material)
-mesh.material.side = THREE.BackSide
-scene.add(mesh)
+const world = new THREE.Mesh(geometry, material)
+world.material.side = THREE.BackSide
+scene.add(world)
 
 /**
  * Particles
@@ -50,21 +58,41 @@ scene.add(mesh)
 
 const particlesGeometry = new THREE.BufferGeometry()
 const particlesMaterial = new THREE.PointsMaterial({
-    size: 0.2,
-    sizeAttenuation: true // to specify if distant particles should be smaller than close particles
+    size: 0.8,
+    sizeAttenuation: true, // to specify if distant particles should be smaller than close particles
+    blending: THREE.AdditiveBlending,
+    map: snowflake,
+    depthTest: false,
+    transparent: true,
 })
-const count = 5000
 
-const positions = new Float32Array(count * 3)
 
-for(let i=0; i<count * 3; i++){
-    positions[i] = (Math.random()- 0.5) * 50
+const addSnowflakes = () => {
+    for(let i=0; i<count * 3; i++){
+        positions.push(
+            Math.floor(Math.random() * maxRange - minRange), // x position fom -500 to 500
+            Math.floor(Math.random() * minRange - minHeight), // y position fom -250 to 750
+            Math.floor(Math.random() * maxRange - minRange) // z position fom -500 to 500
+        )
+        velocities.push(
+            Math.floor(Math.random() * 1.7) * 0.05,
+            Math.floor(Math.random() * 2) * 0.14,
+            Math.floor(Math.random() * 6-3) * 0.05
+        )
+    
+    }
 }
+addSnowflakes()
 
-particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+
+
+
+particlesGeometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3))
+particlesGeometry.setAttribute('velocity', new THREE.Float32BufferAttribute(velocities, 3))
+console.log(particlesGeometry)
 
 // Points
-const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+particles = new THREE.Points(particlesGeometry, particlesMaterial)
 scene.add(particles)
 
 window.addEventListener('resize', () =>
@@ -112,10 +140,27 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
+const updateParticles = () => {
+    for(let i = 0; i < count * 3; i+=3){
+        particles.geometry.attributes.position.array[i] -= particles.geometry.attributes.velocity.array[i]
+        particles.geometry.attributes.position.array[i+1] -= particles.geometry.attributes.velocity.array[i+1]
+        particles.geometry.attributes.position.array[i+2] -= particles.geometry.attributes.velocity.array[i+2]
+
+        if(particles.geometry.attributes.position.array[i+1] < -100){
+            particles.geometry.attributes.position.array[i] =  Math.floor(Math.random() * maxRange - minRange), // x position fom -500 to 500
+            particles.geometry.attributes.position.array[i+1] = Math.floor(Math.random() * minRange - minHeight), // y position fom -250 to 750
+            particles.geometry.attributes.position.array[i+2] = Math.floor(Math.random() * maxRange - minRange) // z position fom -500 to 500
+        }
+    }
+    // To let GPU know it needs to update the positions array we need to write below line
+    particles.geometry.attributes.position.needsUpdate = true;
+}
+
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
+    updateParticles()
     // Update controls
     controls.update()
 
